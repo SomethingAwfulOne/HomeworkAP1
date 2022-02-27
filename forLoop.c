ecfc64b7d644cbd1cea015102d1a24281f2ff814
Hau519 <hau.katolik@gmail.com>
2022-02-26 23:24:16 -0500

    add question 4

--- forLoop.c
+++ forLoop.c
@@ -15,11 +15,17 @@ printf("sum from 1 to 10 is %d\n", sum);
 for (int c = 33; c<=126; c++){
     printf("%c\n",c);
 }
-*/
+
 //3 print number from 0-6 except 3 and 6
 for (int j=0; j<=6; j++){
     if (j!=3&&j!=6)
         printf("%d\n", j);
+}
+*/
+// 4 printf from 1500 to 2700 that divisible by 5 and 7
+for (int l=1500; l<=2700; l+=5){
+    if (l%7==0)
+        printf("%d\n", l);
 }
 
 
