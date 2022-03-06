9b78a22670f8492513f1b8bc447f1a64c91083e5
Hau Vu <65142251+Hau519@users.noreply.github.com>
2022-03-06 14:51:23 -0500

    Update assignment3.c

--- assignment3.c
+++ assignment3.c
@@ -27,7 +27,7 @@ for (int i=1; i<=5; i++){
     }
     printf("\n");
     }
- Another way :- 
+// Another way : 
       for (int i = 1; i <= 5; i++){
         for (int j = 1; j <= 5; j++){
             if(i>j)
