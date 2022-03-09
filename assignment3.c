3bfaf757c8138a34990bcd0050971be63b645a95
Hau519 <hau.katolik@gmail.com>
2022-03-08 22:25:05 -0500

    updated

--- assignment3.c
+++ assignment3.c
@@ -48,9 +48,16 @@ for (int i=1; i<=5; i++){
     }
     printf("\n");
     }
+//6 
+for (int i=1; i<=5; i++){
+    for (int j=0; j<i; j++){
+        printf("%d", 5-j);
+    }
+    printf("\n");
+    }
 //7
- for (int i=1; i<=6; i++){
-    for (int j=1; j<i; j++){
+ for (int i=1; i<=5; i++){
+    for (int j=0; j<i; j++){
         printf("%d", i-j);
     }
     printf("\n");
